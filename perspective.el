01f1027242c4f436e58e09ff50781644aefe44b0
Nathan Weizenbaum <nex342@gmail.com>
2009-01-04 19:48:37 -0800

    Add with-selected-frame for Emacs 22 compat.

--- perspective.el
+++ perspective.el
@@ -5,6 +5,26 @@
 
 (eval-when-compile (require 'cl))
 
+;; This is only available in Emacs >23,
+;; so we redefine it here for compatibility.
+(unless (fboundp 'with-selected-frame)
+  (defmacro with-selected-frame (frame &rest body)
+    "Execute the forms in BODY with FRAME as the selected frame.
+The value returned is the value of the last form in BODY.
+See also `with-temp-buffer'."
+    (declare (indent 1) (debug t))
+    (let ((old-frame (make-symbol "old-frame"))
+          (old-buffer (make-symbol "old-buffer")))
+      `(let ((,old-frame (selected-frame))
+             (,old-buffer (current-buffer)))
+         (unwind-protect
+             (progn (select-frame ,frame)
+                    ,@body)
+           (if (frame-live-p ,old-frame)
+               (select-frame ,old-frame))
+           (if (buffer-live-p ,old-buffer)
+               (set-buffer ,old-buffer)))))))
+
 (defvar persp-initialized nil
   "Non-nil if the perspectives system has been initialized.")
 
