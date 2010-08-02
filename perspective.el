b1845b6ddf632575982a9985a2375494b667eec5
Nathan Weizenbaum <nex342@gmail.com>
2010-08-01 17:06:17 -0700

    Make the switch-to-buffer advice more robust against different argument names.
    Closes gh-2.

--- perspective.el
+++ perspective.el
@@ -545,7 +545,7 @@ is non-nil or with prefix arg, don't switch to the new perspective."
 
 See also `persp-add-buffer'."
   ;; The relevant argument is named BUFFER in Emacs <23 and BUFFER-OR-NAME in Emacs >23
-  (persp-add-buffer (or (bound-and-true-p buffer) buffer-or-name)))
+  (persp-add-buffer (ad-get-arg 0)))
 
 (defadvice recursive-edit (around persp-preserve-for-recursive-edit)
   "Preserve the current perspective when entering a recursive edit."
