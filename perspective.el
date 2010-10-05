66c8b0e1298abf8141fab75c1dbba7f39f2a7afd
Nathan Weizenbaum <nex342@gmail.com>
2010-10-05 12:42:47 -0700

    Bump version to 1.5.

--- perspective.el
+++ perspective.el
@@ -5,7 +5,7 @@
 
 ;; Author: Nathan Weizenbaum
 ;; URL: http://github.com/nex3/perspective-el
-;; Version: 1.4
+;; Version: 1.5
 ;; Created: 2008-03-05
 ;; By: Nathan Weizenbaum
 ;; Keywords: workspace, convenience, frames
