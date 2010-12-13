4f822f1c352cc8005a5cb3c4410a72eed21d6bd5
Nathan Weizenbaum <nweiz@google.com>
2010-12-13 14:16:38 -0800

    Fix the header comment.

--- perspective.el
+++ perspective.el
@@ -1,4 +1,5 @@
-;; perspective.el --- switch between named "perspectives" of the editor
+;;; perspective.el --- switch between named "perspectives" of the editor
+
 ;; Copyright (C) 2008-2010 Nathan Weizenbaum <nex342@gmail.com>
 ;;
 ;; Licensed under the same terms as Emacs.
