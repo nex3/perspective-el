18d2b8cf51ffa40a1dd02569e94dbe92b916c70b
Natalie Weizenbaum <nweiz@google.com>
2015-05-08 16:25:10 -0700

    Respect ido's ignored files.
    Closes #28

--- perspective.el
+++ perspective.el
@@ -788,12 +788,12 @@ it. In addition, if one exists already, runs BODY in it immediately."
         (indices (make-hash-table :test 'equal)))
     (cl-loop for elt in ido-temp-list
              for i upfrom 0
-             do (puthash (copy-sequence elt) i indices))
+             do (puthash elt i indices))
     (setq ido-temp-list
-          (let ((length (length ido-temp-list)))
-            (sort persp-names (lambda (a b)
-                                (< (gethash (copy-sequence a) indices length)
-                                   (gethash (copy-sequence b) indices length))))))))
+          (sort (intersection persp-names ido-temp-list)
+                (lambda (a b)
+                  (< (gethash a indices)
+                     (gethash b indices)))))))
 
 (defun quick-perspective-keys ()
   "Bind quick key commands to switch to perspectives.
