bf42fb8ae16c90f0210e4bcb80800e95b6d90305
Natalie Weizenbaum <nweiz@google.com>
2018-06-13 11:50:16 -0700

    Style change

--- perspective.el
+++ perspective.el
@@ -592,10 +592,9 @@ See also `persp-switch' and `persp-add-buffer'."
          (kill-buffer buffer))
         ;; Make the buffer go away if we can see it.
         ((get-buffer-window buffer)
-         (let (win)
-           (while (setq win (get-buffer-window buffer))
-             (with-selected-window win
-               (bury-buffer)))))
+         (while (get-buffer-window buffer)
+           (with-selected-window (get-buffer-window buffer)
+             (bury-buffer))))
         (t (bury-buffer buffer)))
   (setf (persp-buffers (persp-curr)) (remq buffer (persp-buffers (persp-curr)))))
 
