8fbd467fdddfec32f1e8022c27b5bc099c13ff9b
Ryan Davis <ryand-ruby@zenspider.com>
2019-12-02 14:52:18 -0800

    Remove with-selected-frame definition.
    We're only testing from 24.4 and up, so we should be good here.

--- perspective.el
+++ perspective.el
@@ -97,26 +97,6 @@ save state when exiting Emacs."
   :group 'perspective-mode
   :type 'file)
 
-;; This is only available in Emacs >23,
-;; so we redefine it here for compatibility.
-(unless (fboundp 'with-selected-frame)
-  (defmacro with-selected-frame (frame &rest body)
-    "Execute the forms in BODY with FRAME as the selected frame.
-The value returned is the value of the last form in BODY.
-See also `with-temp-buffer'."
-    (declare (indent 1) (debug t))
-    (let ((old-frame (make-symbol "old-frame"))
-          (old-buffer (make-symbol "old-buffer")))
-      `(let ((,old-frame (selected-frame))
-             (,old-buffer (current-buffer)))
-         (unwind-protect
-             (progn (select-frame ,frame)
-                    ,@body)
-           (if (frame-live-p ,old-frame)
-               (select-frame ,old-frame))
-           (if (buffer-live-p ,old-buffer)
-               (set-buffer ,old-buffer)))))))
-
 (defmacro persp-let-frame-parameters (bindings &rest body)
   "Like `let', but for frame parameters.
 Temporariliy set frame parameters according to BINDINGS then eval BODY.
