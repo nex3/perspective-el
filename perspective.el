53348cea0f46655c4c072da5984f6a652726df4f
Constantine Vetoshev <gepardcv@gmail.com>
2021-09-18 20:17:07 -0700

    Version 2.17.

--- perspective.el
+++ perspective.el
@@ -7,7 +7,7 @@
 ;; Author: Natalie Weizenbaum <nex342@gmail.com>
 ;; URL: http://github.com/nex3/perspective-el
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.5"))
-;; Version: 2.16
+;; Version: 2.17
 ;; Created: 2008-03-05
 ;; By: Natalie Weizenbaum <nex342@gmail.com>
 ;; Keywords: workspace, convenience, frames
